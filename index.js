a2412791ae4e67c52b09fc60630af940d451d7f5
Janet Njeri Ngungu <janetnjerin89@gmail.com>
2022-06-08 21:57:44 +0300

    adding functions lab

--- index.js
+++ index.js
@@ -3,25 +3,21 @@
 
 const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
 
-const returnFirstTwoDrivers = function (drivers) {
-
-    return drivers.slice(0, 2);
-  };
-  const returnLastTwoDrivers = function (drivers) {
+const returnFirstTwoDrivers = function(drivers) {
+  return drivers.slice(0, 2);
+}
+function returnLastTwoDrivers(drivers){
   return drivers.slice(-2);
-};
-const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+}
+let selectingDrivers =[returnFirstTwoDrivers, returnLastTwoDrivers]
+let createFareMultiplier = multiplier => {
+  return value => {
+      return multiplier * value;
+  }
+}
+let fareDoubler = createFareMultiplier(2);
+let fareTripler = createFareMultiplier(3);
 
-const createFareMultiplier = function (multiplyValue) {
-  return function (value) {
-    return multiplyValue * value;
-  };
-};
-
-const fareDoubler = createFareMultiplier(2);
-
-const fareTripler = createFareMultiplier(3);
-
-const selectDifferentDrivers = function (drivers, driversToReturn) {
-  return driversToReturn(drivers);
-};
+let selectDifferentDrivers = (drivers, returnDrivers) => {
+  return returnDrivers(drivers);
+}
